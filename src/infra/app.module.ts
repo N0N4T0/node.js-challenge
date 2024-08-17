00aaf5ccaa4395cb1a6f7bfd37ee75244272c70c
Lucio <luciononato@hotmail.com>
2024-08-17 14:03:21 -0300

    fix: add CacheModule

--- src/infra/app.module.ts
+++ src/infra/app.module.ts
@@ -4,6 +4,7 @@ import { envSchema } from './env/env'
 import { AuthModule } from './auth/auth.module'
 import { HttpModule } from './http/http.module'
 import { EnvModule } from './env/env.module'
+import { CacheModule } from '@nestjs/cache-manager'
 
 @Module({
   imports: [
@@ -11,6 +12,7 @@ import { EnvModule } from './env/env.module'
       validate: (env) => envSchema.parse(env),
       isGlobal: true,
     }),
+    CacheModule.register(),
     AuthModule,
     HttpModule,
     EnvModule,
